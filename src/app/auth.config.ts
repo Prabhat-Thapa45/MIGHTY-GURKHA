8d6930b851c30be61dc3145d23cdf0c89d5eb539
Prabhat-Thapa45 <akoup66@gmail.com>
2025-07-12 09:22:25 +0545

    testing creds

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,15 +2,14 @@ import bcrypt from "bcryptjs";
 import { getUserByEmail } from "./db";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-
 export const authConfig = {
   providers: [
     CredentialsProvider({
-    name: "Credentials",
-    credentials: {
-      email: { label: "Email", type: "text" },
-      password: { label: "Password", type: "password" },
-    },
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
           return null;
@@ -19,7 +18,15 @@ export const authConfig = {
         const user = await getUserByEmail(credentials.email);
         if (!user) return null;
 
-        const isValid = await bcrypt.compare(credentials.password, user.password);
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+        console.log("Credentials:", credentials);
+        console.log("User from DB:", user);
+        console.log("Password match:", isValid);
+        console.log("Is admin:", user?.isAdmin);
+
         if (isValid && user.isAdmin) {
           return user;
         }
